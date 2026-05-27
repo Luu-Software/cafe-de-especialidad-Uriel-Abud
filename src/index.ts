b3dc97828923e82df956e5d49c7fd0d02de4f231
Uriel-Abud <uriabud@gmail.com>
2026-05-27 15:42:19 -0300

    Cambié el nombre de la variable "totalNúmero" a "cuentaNúmero".

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 import { preguntar } from './lib/consola.ts';
 
 let totalTexto: string = preguntar('Total de la cuenta: ');
-let totalNúmero: number = Number(totalTexto);
+let cuentaNúmero: number = Number(totalTexto);
 
 let esEstudiante: string = preguntar('¿Son estudiantes de ORT? si/no: ');
 if (esEstudiante === "si") {
-    totalNúmero = totalNúmero * 0.9;
+    cuentaNúmero = cuentaNúmero * 0.9;
 }
 
 let personasTexto: string = preguntar('Dividir la cuenta entre: ');
 let personasNúmero: number = Number(personasTexto);
 
-let totalPorPersona: number = totalNúmero / personasNúmero;
+let totalPorPersona: number = cuentaNúmero / personasNúmero;
 console.log("Total por persona: $" + totalPorPersona);
